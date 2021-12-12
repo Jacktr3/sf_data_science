5be175c84c626989070613610e3ea4fce27ae410
jacktr3 <jackt333@mail.ru>
2021-12-12 22:22:48 +0300

    edit game_v3.py

--- project_0/game_v3.py
+++ project_0/game_v3.py
@@ -2,7 +2,9 @@ import numpy as np
 
 
 def game_core_v3(number: int = 1) -> int:
-    """
+    """Сначала задаем число, середина диапозона поиска числа, а потом уменьшаем
+    или увеличиваем его в зависимости от того, больше оно или меньше загаданного числа.
+       Функция принимает загаданное число и возвращает число попыток за которое алгоритм определил загаданное число arrow_up в readme
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
 
@@ -13,21 +15,28 @@ def game_core_v3(number: int = 1) -> int:
 
     count = 0   # счетчик попыток
     prdict_number_min = 1  # нижняя граница поиска числа
-    prdict_number_max = 101  # верхняя граница поиска числа
+    prdict_number_max = 100  # верхняя граница поиска числа
+    predict_number = (prdict_number_max + prdict_number_min) // 2 # предполагаемое число
+   
     
-    while True:
+    while number!=predict_number:
         count+=1
         
-        predict_number = (prdict_number_max + prdict_number_min) // 2
-
         if number > predict_number:
-            prdict_number_min = predict_number  # смещение нижней границы поиска числа
+            if number-predict_number > 20:
+                predict_number+=20      # увеличиваем предполагаемое число на 20
+            elif number-predict_number <= 20 and number-predict_number >= 10:
+                predict_number+=10      # увеличиваем предполагаемое число на 10
+            elif number-predict_number < 10:
+                predict_number+=1       # увеличиваем предполагаемое число на 1
 
         elif number < predict_number:
-            prdict_number_max = predict_number  # смещение верхней границы поиска числа
-
-        else:
-            break
+            if predict_number-number > 20:
+                predict_number-=20      # уменьшаем предполагаемое число на 20
+            elif predict_number-number <= 20 and predict_number-number >= 10:
+                predict_number-=10      # уменьшаем предполагаемое число на 10
+            elif predict_number-number < 10:
+                predict_number-=1       # уменьшаем предполагаемое число на 1
 
     # Ваш код заканчивается здесь
 
@@ -36,14 +45,14 @@ def game_core_v3(number: int = 1) -> int:
 def score_game(game_core_v3) -> int:
     """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
     Args:
-        random_predict ([type]): функция угадывания
+        game_core_v3 ([type]): функция угадывания
     Returns:
         int: среднее количество попыток
     """
 
     count_ls = []  # список для сохранения количества попыток
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(10000))  # загадали список чисел
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
         count_ls.append(game_core_v3(number))
@@ -51,6 +60,11 @@ def score_game(game_core_v3) -> int:
     score = int(np.mean(count_ls))  # находим среднее количество попыток
 
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
-    return(score)
+    return score
 
+if __name__ == '__main__':
+    # RUN
+    score_game(game_core_v3)
+
+print('Run benchmarking for game_core_v3: ', end='')
 score_game(game_core_v3)
